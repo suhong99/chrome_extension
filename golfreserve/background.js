ab952e3a55fede2cf67f9081002eed2ea2712776
SUHONG\topic <topic828@Naver.com>
2024-12-21 16:47:21 +0900

    예약까지

--- golfreserve/background.js
+++ golfreserve/background.js
@@ -1,12 +1,5 @@
 chrome.commands.onCommand.addListener((command) => {
   if (command === 'start_reservation') {
-    chrome.notifications.create({
-      type: 'basic',
-      iconUrl: '/images/icons-32.png',
-      title: '알림',
-      message: 'Alt+T가 눌렸습니다!',
-    });
-
     chrome.storage.local.get(['startTime', 'endTime'], (data) => {
       const { startTime, endTime } = data;
 
@@ -47,6 +40,28 @@ chrome.commands.onCommand.addListener((command) => {
       });
     });
   }
+
+  // Alt + U 커맨드 처리
+  if (command === 'fill_cert_no') {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length === 0) {
+        chrome.notifications.create({
+          type: 'basic',
+          iconUrl: '/images/icons-32.png',
+          title: '알림',
+          message: '활성화된 탭이 없습니다.',
+        });
+        return;
+      }
+
+      const tabId = tabs[0].id;
+
+      chrome.scripting.executeScript({
+        target: { tabId },
+        func: fillCertNo,
+      });
+    });
+  }
 });
 
 function executeReservation(startNumeric, endNumeric) {
@@ -63,10 +78,36 @@ function executeReservation(startNumeric, endNumeric) {
         buttons.forEach((button) => {
           if (!button.disabled) {
             button.click();
-            console.log(`예약 버튼 클릭됨: ${timeText}`);
           }
         });
       }
     }
   });
+}
+
+function fillCertNo() {
+  const certNoElement = document.getElementById('golfTimeDiv2CertNo');
+  if (certNoElement) {
+    const certNo = certNoElement.textContent.trim();
+
+    // certNoChk 입력 필드에 값 설정
+    const inputField = document.getElementById('certNoChk');
+    if (inputField) {
+      inputField.value = certNo;
+      console.log(`certNo (${certNo})가 입력 필드에 설정되었습니다.`);
+
+      // 입력이 완료된 후 예약 버튼 클릭
+      const reservationButton = document.querySelector('.btn.btn-res03');
+      if (reservationButton) {
+        reservationButton.click();
+        console.log('예약 버튼이 클릭되었습니다.');
+      } else {
+        console.error('예약 버튼을 찾을 수 없습니다.');
+      }
+    } else {
+      console.error('certNoChk 입력 필드를 찾을 수 없습니다.');
+    }
+  } else {
+    console.error('golfTimeDiv2CertNo 요소를 찾을 수 없습니다.');
+  }
 }
